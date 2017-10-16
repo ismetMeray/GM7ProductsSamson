d9997519521f8d14133b328688c3403a0cf34089
Erin Henson <hellcrafter@live.com>
2017-10-16 16:47:39 +0200

    selectRowData functie gemaakt, IDs toegevoegd aan td's in table

--- index.php
+++ index.php
@@ -18,31 +18,28 @@ $(document).ready(function(){
     });
 
     $(".input-bx input").on('blur',function(){
-      if($(this).val()!=""){
+      if($(this).val()!="")
+      {
         $(this)
           .addClass('input-bx-focus-out-true')
           .removeClass('input-bx-focus-out-false');
-      }else{
+      }
+      else
+      {
         $(this)
           .addClass('input-bx-focus-out-false')
           .removeClass('input-bx-focus-out-true');
       }
-
     });
 $('#login-modal').modal('show');
 });
-
-
-$("button").click(function() {
-    //alert(this.id);
-    alert($(this).attr('id'));
-});
 </script>
+
 </head>
 
 <body>
+<br/>
 <center>
-<br />
 <div id="form">
 <form class="form-horizontal" method="post">
 <fieldset>
@@ -71,14 +68,12 @@ $("button").click(function() {
     </div>
 </div>
 <?php
-if(isset($_GET['edit']))
-{
+if(isset($_GET['edit'])){
  ?>
  <button type="submit" name="update">update</button>
  <?php
 }
-else
-{
+ else{
  ?>
  <button type="submit" name="save">save</button>
  <?php
@@ -86,26 +81,45 @@ else
 ?>
 </fieldset>
 </form>
-
+<center>
 <br /><br />
 
 <table width="100%" border="1" cellpadding="15" align="center">
+  <script>
+  var selectRowData = function(clicked_id){
+    var serial = "";
+    var productName = "";
+    var date = "";
+    var comment = "";
+
+      serial = document.getElementById('serial' + clicked_id).innerHTML;
+      productName = document.getElementById('productName' + clicked_id).innerHTML;
+      date = document.getElementById('date' + clicked_id).innerHTML;
+      comment = document.getElementById('comment' + clicked_id).innerHTML;
+      console.log(productName);
+  };
+  </script>
 <?php
-$res = $MySQLiconn->query("SELECT * FROM Producten");
+$result = $MySQLiconn->query("SELECT * FROM Producten");
 $i = 0;
-while($row=$res->fetch_array())
+while($row=$result->fetch_array())
 {
-    $i++
+    $i++;
  ?>
     <tr>
-    <td><?php echo $row['Serienummer']; ?></td>
-    <td><?php echo $row['Product']; ?></td>
-    <td><?php echo $row['Productiedatum']; ?></td>
-    <td><?php echo $row['Opmerking']; ?></td>
-    <td><button id="<?php echo $i ?>" data-toggle="modal" data-target="#myModal" onclick="alert(this.id)">edit</button></td>
-    <td><a href="?del=<?php echo $row['Serienummer']; ?>" onclick="return confirm('sure to delete !'); " >delete</a></td>
+    <td id="serial<?php echo $i; ?>"> <?php echo $row['Serienummer']; ?></td>
+    <td id="productName<?php echo $i; ?>"> <?php echo $row['Product']; ?></td>
+    <td id="date<?php echo $i; ?>"> <?php echo $row['Productiedatum']; ?></td>
+    <td id="comment<?php echo $i; ?>"> <?php echo $row['Opmerking']; ?></td>
+    <td>
+      <button id="<?php echo $i ?>" data-toggle="modal" data-target="#myModal" onClick="selectRowData('<?php echo $i ?>')" >Edit</button>
+
+    </td>
+    <td>
+      <a href="?del=<?php echo $row['Serienummer']; ?>" onClick="return confirm('Confirm Delete!')"> Delete</a>
+    </td>
     </tr>
-    <?php
+<?php
 }
 ?>
 <div class="modal  fade" id="myModal"  >
@@ -118,22 +132,18 @@ while($row=$res->fetch_array())
         <div class="modal-body">
             <div id="login">
                 <section class="Product-toevoegen" >
-
                             <label class="labelCss" for="form_user">Serienummer</label>
                         <div class="input-bx">
                         <input type="text" name="Serienummer" class="txtField" value="<?php echo $row['Serienummer']; ?>">
                         </div>
-
                             <label class="labelCss" for="form_pass">Product</label>
                         <div class="input-bx">
                             <input id="form_pass" name="form_pass"   type="text" class="form-control" required="" />
                         </div>
-
                             <label class="labelCss" for="form_pass">Productiedatum</label>
                                 <div class='right-inner-addon date datepicker' data-date-format="dd-mm-yyy">
         		                    <input name='name' value="<?php ?>" type="date" class="form-control date-picker" data-date-format="dd-mm-yyy" style="text-align: center"/>
       			             </div>
-
                             <label class="labelCss" for="form_pass">Opmerking</label>
                         <div class="input-bx">
                             <textarea id="form_pass" name="form_pass" type="text" class="form-control" required=""></textarea>
@@ -143,7 +153,6 @@ while($row=$res->fetch_array())
                          <!--<button type="button" id="register" class="btn btn-primary"><span class="glyphicon glyphicon-hand-right"></span> Product Toevoegen</button>-->
                             <a href="?edit=<?php echo $row['Serienummer']; ?>" class="btn btn-primary" onclick="return confirm('sure to update !'); " >Product Wijzigen</a>
                        </div><!-- /.modal-footer -->
-
 
                 </section>
             </div>
@@ -156,6 +165,5 @@ while($row=$res->fetch_array())
 
 </table>
 </div>
-</center>
 </body>
 </html>
